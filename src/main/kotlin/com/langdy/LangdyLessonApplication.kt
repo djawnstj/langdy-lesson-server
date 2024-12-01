9ffe715e8603f301b1707307e7754c8655087052
junseo <djawnstj44@naver.com>
2024-12-01 16:03:18 +0900

    feat: SpringBoot 프로젝트 초기 세팅

---
+++ src/main/kotlin/com/langdy/LangdyLessonApplication.kt
@@ -0,0 +1,11 @@
+package com.langdy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LangdyLessonApplication
+
+fun main(args: Array<String>) {
+    runApplication<LangdyLessonApplication>(*args)
+}
---
+++ src/test/kotlin/com/langdy/LangdyLessonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.langdy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LangdyLessonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
