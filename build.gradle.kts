c95f711928c0e49eb0a246a46375b8ce2d07f13c
junseo <djawnstj44@naver.com>
2024-12-04 00:09:55 +0900

    chore: build.gradle 의존성, JPA 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,16 @@ plugins {
     kotlin("plugin.jpa") version "1.9.25"
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+}
+
 group = "com.langdy"
 version = "0.0.1-SNAPSHOT"
 
@@ -20,14 +30,29 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // Spring Web
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // DB
+    runtimeOnly("com.h2database:h2")
+
+    // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
+
+    // 테스트
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.11")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+
+    // kotest
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.0")
+    testImplementation("io.kotest:kotest-framework-datatest:5.9.0")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
 }
 
 kotlin {
