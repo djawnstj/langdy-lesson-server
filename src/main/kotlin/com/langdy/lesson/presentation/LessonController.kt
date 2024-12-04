280832695fa8b44aa89f0287b76df48da0688d99
junseo <djawnstj44@naver.com>
2024-12-04 22:14:04 +0900

    feat: 수업 취소 api 추가

--- src/main/kotlin/com/langdy/lesson/presentation/LessonController.kt
+++ src/main/kotlin/com/langdy/lesson/presentation/LessonController.kt
@@ -4,9 +4,12 @@ import com.langdy.auth.application.UserDetails
 import com.langdy.auth.presentation.LoginUser
 import com.langdy.lesson.presentation.dto.request.EnrollLessonRequest
 import com.langdy.lesson.application.LessonCommandService
+import com.langdy.lesson.application.command.CancelLessonCommand
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,5 +22,11 @@ class LessonController(
     @ResponseStatus(HttpStatus.CREATED)
     fun enrollLesson(@LoginUser user: UserDetails, @RequestBody request: EnrollLessonRequest) {
         lessonCommandService.enroll(request.toCommand(user.id))
+    }
+
+    @DeleteMapping("/lessons/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun cancelLesson(@LoginUser user: UserDetails, @PathVariable id: Long) {
+        lessonCommandService.cancel(CancelLessonCommand(id, user.id))
     }
 }
