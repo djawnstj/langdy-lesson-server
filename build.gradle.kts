04e41a0fb0bb74b138a764c2ed650ca781276c49
junseo <djawnstj44@naver.com>
2024-12-05 22:32:26 +0900

    feat: 레디스 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
     // DB
     runtimeOnly("com.h2database:h2")
 
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
     // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
